219916645c7d44bcdf282585de481d6212f538fe
nojick <nojick@gmail.com>
2021-10-13 17:30:57 +0300

    ido: DRM tui_comm

--- ido-vendor.mk
+++ ido-vendor.mk
@@ -526,6 +526,12 @@ PRODUCT_COPY_FILES += \
 	vendor/xiaomi/ido/proprietary/vendor/etc/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
 	vendor/xiaomi/ido/proprietary/vendor/etc/init/vendor.qti.hardware.perf@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.perf@1.0-service.rc
 
+# DRM TUI
+PRODUCT_COPY_FILES += \
+	vendor/xiaomi/ido/proprietary/vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti \
+	vendor/xiaomi/ido/proprietary/vendor/etc/init/vendor.qti.hardware.tui_comm@1.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.tui_comm@1.0-service-qti.rc \
+	vendor/xiaomi/ido/proprietary/vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.tui_comm@1.0.so
+
 PRODUCT_PACKAGES += \
     libtime_genoff \
     TimeService \
